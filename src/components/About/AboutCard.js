314212ea206b7d227a3455f1a6ba2d6fa59a2660
gauravshukla2203 <gaurav.shukla.gs48@gmail.com>
2023-02-24 02:29:39 +0530

    created some spaces

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,8 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Gaurav Shukla</span>
-            from <span className="purple"> Shahdol, India.</span>
+            Hi Everyone, I am <span className="purple"> Gaurav Shukla </span> 
+            from  <span className="purple">  Shahdol, India.</span>
             <br />I am a developer pursuing BTech in Jaypee University of Engineering and Technology.
             <br />
             <br />
