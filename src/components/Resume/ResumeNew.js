f46f205ffc3df30dca5dbcfc26789a3228e322fa
gauravshukla2203 <gaurav.shukla.gs48@gmail.com>
2023-02-24 02:38:52 +0530

    resolved some errors

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://drive.google.com/file/d/1d7rXB7gm6M_PwZqtMbtQZrpD_fnXl2Ev/view?usp=share_link";
+  "https://raw.githubusercontent.com/withgaurav/portfolio/master/src/Assets/GAURAV_SHUKLA.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
